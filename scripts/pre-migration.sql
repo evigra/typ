ac7b67f016f3ae83d55c1d92fa1b6f0d63644d93
Jose Morales <jose@vauxoo.com>
2016-04-16 02:59:15 +0000

    [IMP] Modified the order to delete the views

--- scripts/pre-migration.sql
+++ scripts/pre-migration.sql
@@ -1,6 +1,5 @@
 UPDATE stock_inventory_line SET product_uom=113 WHERE id=188465;
 UPDATE stock_move SET product_uom=113 WHERE id=846516;
-DELETE FROM ir_ui_view WHERE arch ilike '%xpath%button%429%';
 DROP FUNCTION IF EXISTS delete_old_views(view ir_ui_view);
 CREATE OR REPLACE FUNCTION delete_old_views(view ir_ui_view)
 RETURNS varchar AS $$
@@ -22,3 +21,4 @@ SELECT delete_old_views(views) AS name FROM ir_ui_view AS views WHERE arch ilike
 SELECT delete_old_views(views) AS name FROM ir_ui_view AS views WHERE arch ilike '%procurement_id%' AND model='stock.warehouse.orderpoint';
 SELECT delete_old_views(views) AS name FROM ir_ui_view AS views WHERE arch ilike '%xpath%string%Expected Date%' AND model='purchase.order';
 SELECT delete_old_views(views) AS name FROM ir_ui_view AS views WHERE arch ilike '%required_date_product%' AND model='purchase.order';
+DELETE FROM ir_ui_view WHERE arch ilike '%xpath%button%429%';
