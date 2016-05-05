9ca4f9360f9b57c9c13b0589be4ebf20de0f5a1b
Jose Morales <jose@vauxoo.com>
2016-05-05 16:58:49 +0000

    [IMP] Honestly I do not understand what are you trying to do here. I commented the code where you make reference to removed columns of the table stock_warehouse

--- argil_multi_warehouse_view/stock.py
+++ argil_multi_warehouse_view/stock.py
@@ -34,7 +34,7 @@ import openerp.addons.decimal_precision as dp
 class stock_warehouse_view(osv.osv):
     _name = 'stock.warehouse_view'
     _description = "Warehouse without Company_id restriction"
-    
+
     _columns = {
             'name'          : fields.char('Name', size=128),
             'lot_input_id'  : fields.integer('Input Location'),
@@ -42,9 +42,9 @@ class stock_warehouse_view(osv.osv):
             'lot_stock_id'  : fields.integer('Stock Location'),
             'partner_id'    : fields.integer('Partner'),
             }
-    
+
     _order = 'name'
-    
+
 stock_warehouse_view()
 
 ##############################
@@ -55,7 +55,7 @@ class stock_warehouse(osv.osv):
 
     def create(self, cr, uid, vals, context=None):
         res = super(stock_warehouse, self).create(cr, uid, vals, context)
-        sql = """insert into stock_warehouse_view 
+        sql = """insert into stock_warehouse_view
                         (id, name, lot_input_id, lot_output_id, lot_stock_id, partner_id, create_uid, write_uid, create_date, write_date)
                     values (%s, '%s', %s, %s, %s, %s, %s, %s, '%s', '%s')""" % \
             (res, vals['name'],vals['lot_input_id'], vals['lot_output_id'], vals['lot_stock_id'], vals['partner_id'] or 'NULL',
@@ -63,34 +63,34 @@ class stock_warehouse(osv.osv):
         #print "sql: ", sql
         cr.execute(sql)
         return res
-        
-    def write(self, cr, uid, ids, vals, context=None):        
+
+    def write(self, cr, uid, ids, vals, context=None):
         res = super(stock_warehouse, self).write(cr, uid, ids, vals, context=context)
         cr.execute("""select id from stock_warehouse_view where id in (%s)""" % ((tuple(ids,))))
         data = cr.fetchall()
-        if not data:
-            for rec in self.browse(cr,uid, ids):
-                sql = """insert into stock_warehouse_view 
-                            (id, name, lot_input_id, lot_output_id, lot_stock_id, partner_id, create_uid, write_uid, create_date, write_date)
-                        values (%s, '%s', %s, %s, %s, %s, %s, %s, '%s', '%s')""" % \
-                    (rec.id, rec.name,rec.lot_input_id.id, rec.lot_output_id.id, rec.lot_stock_id.id, rec.partner_id.id or 'NULL',
-                     uid, uid, time.strftime(DEFAULT_SERVER_DATETIME_FORMAT), time.strftime(DEFAULT_SERVER_DATETIME_FORMAT))
-        else:
-            sql = "update stock_warehouse_view set write_uid=%s,write_date='%s'" % (uid, time.strftime(DEFAULT_SERVER_DATETIME_FORMAT))
-            datos = {}
-            #print "vals: ", vals
-            for val in vals:
-                if val not in ['name', 'lot_input_id', 'lot_output_id', 'lot_stock_id', 'partner_id']:
-                    continue
-                #print "val: ", val
-                sql += (", ") + (" %s = %s" % (val, ("'" + str(vals[val]) + "'") if vals[val] else 'NULL'))
-            sql += " where id in (%s)"  % ((tuple(ids,)))
-            #print "sql: ", sql
-        #print sql
-        cr.execute(sql)
+        # if not data:
+        #     for rec in self.browse(cr,uid, ids):
+        #         sql = """insert into stock_warehouse_view
+        #                     (id, name, lot_input_id, lot_output_id, lot_stock_id, partner_id, create_uid, write_uid, create_date, write_date)
+        #                 values (%s, '%s', %s, %s, %s, %s, %s, %s, '%s', '%s')""" % \
+        #             (rec.id, rec.name,rec.lot_input_id.id, rec.lot_output_id.id, rec.lot_stock_id.id, rec.partner_id.id or 'NULL',
+        #              uid, uid, time.strftime(DEFAULT_SERVER_DATETIME_FORMAT), time.strftime(DEFAULT_SERVER_DATETIME_FORMAT))
+        # else:
+        #     sql = "update stock_warehouse_view set write_uid=%s,write_date='%s'" % (uid, time.strftime(DEFAULT_SERVER_DATETIME_FORMAT))
+        #     datos = {}
+        #     #print "vals: ", vals
+        #     for val in vals:
+        #         if val not in ['name', 'lot_input_id', 'lot_output_id', 'lot_stock_id', 'partner_id']:
+        #             continue
+        #         #print "val: ", val
+        #         sql += (", ") + (" %s = %s" % (val, ("'" + str(vals[val]) + "'") if vals[val] else 'NULL'))
+        #     sql += " where id in (%s)"  % ((tuple(ids,)))
+        #     #print "sql: ", sql
+        # #print sql
+        # cr.execute(sql)
         return res
 
-    
+
     def unlink(self, cr, uid, ids, context=None):
         res = super(stock_warehouse, self).unlink(cr, uid, ids, context=context)
         sql = """delete from stock_warehouse_view where id in (%s)""" % ((tuple(ids,)))
