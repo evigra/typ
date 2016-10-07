a34b1df4fa780e70928586eae0d6d2de5eabebca
Deivis Laya <deivis@vauxoo.com>
2016-10-07 19:39:53 +0000

    [ADD] typ_purchase: Adding typ_purchase module
    * [ADD] Adding Definition of model
    * [ADD] Adding Definition of model
    * [ADD] Adding filter for source document in view tree of purchase.order model
    * [ADD] Adding README.rst file

---
+++ typ_purchase/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+############################################################################
+#    Module Writen For Odoo, Open Source Management Solution
+#
+#    Copyright (c) 2011 Vauxoo - http://www.vauxoo.com
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#    coded by: Deivis Laya <deivis@vauxoo.com>
+#    planned by: Julio Serna <julio@vauxoo.com>
+############################################################################
+{
+    'name': "Typ Purchase",
+    'license': "LGPL-3",
+    'author': 'Vauxoo',
+    'website': 'http://www.vauxoo.com/',
+    'category': 'Purchase',
+    'version': '8.0.0.1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'purchase',
+    ],
+
+    # always loaded
+    'data': [
+        'views/purchase_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ typ_purchase/models/purchase_order.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models
+
+
+class PurchaseOrder(models.Model):
+
+    _inherit = "purchase.order"
