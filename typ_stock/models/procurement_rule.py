721f465f17329b1d0f059ba6331b0551480a0a44
JulioSerna <julio@vauxoo.com>
2016-10-13 06:16:17 +0000

    [IMP] typ_stock: in order to allow to user without access in another warehouse when pule rule are propagate can be create the procurement e.g. 1. Pule Rule: Resupply from another warehouse making purchase order 2. User A create sale order in warehouse A with route to make resupply from warehouse B in this warehouse user A has no access and we need to make procurement order 3. it will be created this procurement:     - Procurement in warehouse B to make purchase order     - Procurement in warehouse B to make delivery     - Procurement in warehouse A to make reception from transit location     - Procurement in warehouse A to male delivery to customer

--- typ_stock/models/procurement_rule.py
+++ typ_stock/models/procurement_rule.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import fields, models
+from openerp import fields, models, api
 
 
 class ProcurementRule(models.Model):
@@ -12,3 +12,20 @@ class ProcurementRule(models.Model):
         help="When this field is check, the first stock move which is"
         " transferred triggers automatically the transfer to the other"
         " stock move related")
+
+
+class ProcurementOrder(models.Model):
+
+    _inherit = 'procurement.order'
+
+    @api.model
+    def _search_suitable_rule(self, procurement, domain):
+        """This method it is overwrite in order to allow propagate pule rule
+        from warehouse to another when the user has not access.
+        e.g. The user A have access only into warehouse A but when create
+        one sale order with route that make purchase order to warehouse B and
+        resupply the warehouse A we need to propagate procurement to
+        warehouse B with procurement.sudo()"""
+
+        return super(ProcurementOrder, self)._search_suitable_rule(
+            procurement.sudo(), domain)
