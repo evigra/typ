888c788f7937dc89023cda3e57d942985d79f72f
ysantiago <yennifer@vauxoo.com>
2016-11-02 01:51:29 -0400

    [REF] typ_sale: Add test over origin field in purchase and picking when are created from sale order. (#107)

--- typ_sale/tests/__init__.py
+++ typ_sale/tests/__init__.py
@@ -4,3 +4,4 @@ from . import test_purchase_from_sale_order
 from . import test_warehouse_configuration_partner
 from . import test_sale_type_payment_term
 from . import test_invoice_type_payment_term
+from . import test_origin_field_purchase_and_picking
