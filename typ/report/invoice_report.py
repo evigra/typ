d203a864e991a72bd3f29f266f4115a754cd7812
Erick Birbe <erickcion@gmail.com>
2016-11-05 19:04:50 -0400

    [IMP] typ: Modifying model of PDF invoice (#119)
    * [DEL] typ: Remove fields from the PDF invoice report
    
    * [IMP] typ: New fields and distribution for PDF invoice
    
    * [IMP] typ: New fields and distribution for PDF invoice
    
    * [IMP] typ: Modifying model of PDF invoice
    
    The invoice in PDF format was modified adding new elements to the
    document and refactoring some of the others already present in the
    invoice.
    
    * [IMP] typ: Left side perpendicular caption text for invoice PDF
    
    * [REF] typ: Moving Issued field from bottom to top side
    
    * [ADD] typ: Translations for PDF invoice
    
    * [FIX] typ: Removing conditional that hides warranty caption
    
    * [FIX] typ: Fix how we get invoice customs info into PDF file
    
    * [REF] typ: Moving logo of PDF to the left
    
    Fix Issue about getting the Customs info

--- typ/report/invoice_report.py
+++ typ/report/invoice_report.py
@@ -20,9 +20,38 @@ class InvoiceReport(Parser):
         self.localcontext.update({
             'eval': eval,
             'datetime': datetime,
+            'get_model_object': self._get_model_object,
+            'get_sale_order': self._get_sale_order,
+            'get_type_payment_term': self._get_type_payment_term,
             '_': _,
         })
         self.context = context
+
+    def _get_model_object(self, o):
+        """Returns the Record related to the source model object. For example,
+        the account.invoice object associated to the report."""
+        model_obj = None
+        if o.id_source:
+            model_obj = self.pool.get(o.model_source).browse(
+                self.cr, self.uid, o.id_source)
+        return model_obj
+
+    def _get_sale_order(self, id_invoice):
+        """Returns the sale object Record associated to the invoice"""
+        order_brw = self.pool.get('sale.order').search(
+            self.cr, self.uid, [('invoice_ids', 'in', [id_invoice])])
+        id_order = order_brw[0] if len(order_brw) else False
+        order_obj = None
+        if id_order:
+            order_obj = self.pool.get('sale.order').browse(
+                self.cr, self.uid, id_order)
+        return order_obj
+
+    def _get_type_payment_term(self, invoice):
+        """Returns the type payment term in readable form"""
+        tpt = invoice.type_payment_term
+        selection = dict(invoice._columns['type_payment_term'].selection)
+        return _(selection[tpt])
 
 
 class InvoiceReportPDF(osv.AbstractModel):
