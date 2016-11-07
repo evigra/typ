846a036dd4fd9523f26619ab0d20aa2dfa9d1356
Julio [Vauxoo] <julio@vauxoo.com>
2016-11-07 17:48:22 -0600

    [IMP] typ_purchase: change fields char by related (#127)
    * [IMP] typ_purchase: change fields char by related
    
    * [IMP] typ_purchase: get store to allow "group by" in this field

--- typ_purchase/models/purchase_order.py
+++ typ_purchase/models/purchase_order.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class PurchaseOrder(models.Model):
@@ -10,9 +10,9 @@ class PurchaseOrder(models.Model):
     shipment_date = fields.Datetime('order shipment date',
                                     help="this is used to indicate when "
                                     "products ships from supplier warehouse")
-    buyer = fields.Char('Buyer', help="This is the buyer in charge of "
-                        "the order's supplier")
-
-    @api.onchange('partner_id')
-    def _onchange_buyer(self):
-        self.buyer = self.partner_id.buyer_id.name
+    buyer = fields.Many2one(
+        related='partner_id.buyer_id',
+        relation='res.users',
+        readonly=True,
+        store=True,
+        help="This is the buyer in charge of the order's supplier")
