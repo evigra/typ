909cd77e4bd1689f618b0ad127f75cd0aa5456e8
Deivis Laya <deivis@vauxoo.com>
2016-11-15 17:04:03 +0000

    [ADD] typ_default_warehouse_from_sale_team: Adding _get_landed_cost_warehouse_id method
    Adding method for warehouse default in stock.landed.cost.guide model

--- typ_default_warehouse_from_sale_team/models/stock_landed_cost_guide.py
+++ typ_default_warehouse_from_sale_team/models/stock_landed_cost_guide.py
@@ -1,12 +1,17 @@
 # coding: utf-8
 
-from openerp import api, models
+from openerp import api, fields, models
 
 
 class StockLandedCostGuides(models.Model):
 
     _name = 'stock.landed.cost.guide'
-    _inherit = ['stock.landed.cost.guide', 'default.warehouse']
+    _inherit = 'stock.landed.cost.guide'
+
+    @api.multi
+    def _get_landed_cost_warehouse_id(self):
+        sale_team = self.env.user.default_section_id
+        return sale_team.default_warehouse.id
 
     @api.onchange('warehouse_id')
     def onchange_warehouse_id(self):
@@ -19,3 +24,11 @@ class StockLandedCostGuides(models.Model):
                 ('default_warehouse', '=', warehouse)]).journal_guide_id
             self.journal_id = journal_id
         return res
+
+    warehouse_id = fields.Many2one(
+        'stock.warehouse',
+        string='Warehouse',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+        default=_get_landed_cost_warehouse_id,
+        help='Warehouse which this guide belongs to')
