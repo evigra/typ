20a6b255bd7b58924461054444fabf3e56d4f471
jorgescalona <escajorge@gmail.com>
2017-01-02 22:00:55 +0000

    [ADD] typ: Add ir config parameter to price list

--- typ/__openerp__.py
+++ typ/__openerp__.py
@@ -69,6 +69,7 @@
         'data/typ_paper_format.xml',
         'data/server_actions.xml',
         'data/set_configuration.yml',
+        'data/ir_config_parameter.xml',
         'data/typ_security.xml',
         'report/invoice_report.xml',
         'report/report_invoice.xml',
