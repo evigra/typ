b13f75801b6a253e1b2c85541c2b0ce8a7b169ec
Erick Birbe <erick@vauxoo.com>
2017-01-05 05:37:17 +0000

    [ADD] typ: Server action to fix headers of XML without encoding
    This server action helps to fix XML files generated without encoding
    attribute into the header `xml` tag.
    
    Related to VX#3138 T#6690

--- typ/models/ir_attachment_facturae_mx.py
+++ typ/models/ir_attachment_facturae_mx.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import base64
+import xml.dom.minidom
+
 from openerp import models, fields, api
 
 
@@ -19,3 +22,23 @@ class IrAttachmentFacturaeMx(models.Model):
                 continue
             invoice = self.env['account.invoice'].browse(self.id_source)
             record.report_lang = invoice.partner_id.lang
+
+    @api.multi
+    def fix_xml_encoding(self):
+        """This method is a workaround to fix XML files which were returned
+        without encoding attribute after being signed with Vauxoo's PAC. It
+        goes through XML files and write the encoding attribute where
+        necessary"""
+        for attach in self:
+            if not attach.file_xml_sign:
+                continue
+            file_signed = attach.file_xml_sign
+            data = base64.decodestring(file_signed.datas)
+            if data[20:36] == 'encoding="utf-8"':
+                continue
+            document = xml.dom.minidom.parseString(data).toxml(
+                encoding='utf-8').decode('utf-8').encode(
+                    'ascii', 'xmlcharrefreplace').encode('base64')
+            file_signed.write({
+                'datas': document,
+            })
