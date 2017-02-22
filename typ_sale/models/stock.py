4f4c5d7f8a00a04e0b18a0e2bcde76e2f8b1c1a6
ysantiago <yennifer@vauxoo.com>
2017-02-22 14:05:02 -0400

    [REF] typ_sale: Add validation in inherited function _get_invoice_vals. (#302)

--- typ_sale/models/stock.py
+++ typ_sale/models/stock.py
@@ -11,9 +11,11 @@ class StockPicking(models.Model):
     def _get_invoice_vals(self, key, inv_type, journal_id, move):
         res = super(StockPicking, self)._get_invoice_vals(
             key, inv_type, journal_id, move)
-        res.update({
-            'payment_term': move.picking_id.sale_id.payment_term.id,
-            'type_payment_term': move.picking_id.sale_id.type_payment_term, })
+        sale = move.picking_id.sale_id
+        if sale and inv_type in ('out_invoice', 'out_refund'):
+            res.update({
+                'payment_term': sale.payment_term.id,
+                'type_payment_term': sale.type_payment_term, })
         return res
 
 
