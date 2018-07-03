b89f0223bfd2d6936ec8654204827bb69e87424a
JulioSerna <julio@vauxoo.com>
2018-07-03 18:09:40 +0000

    [IMP] typ_landed_cost: deprecate use of the creation of discrete and landed by exchange rate in customs and pruchase

--- typ_landed_costs/models/account_invoice.py
+++ typ_landed_costs/models/account_invoice.py
@@ -6,16 +6,6 @@ from openerp import api, fields, models
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    exchange_landed_ids = fields.Many2many(
-        'stock.landed.cost',
-        'landed_invoice_rel',
-        'invoice_id', 'landed_id',
-        copy=False,
-        string='Exchange Differential Landed',
-        help='Landed created to adjust '
-        'the cost of the product by '
-        'exchange differential')
-
     @api.model
     def line_get_convert(self, line, part, date):
         res = super(AccountInvoice, self).line_get_convert(line, part, date)
