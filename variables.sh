90e24d7dd117857376a6ff597ea5d575996dca04
angelfentanez <joseangel@vauxoo.com>
2018-12-19 10:52:39 -0600

    [ADD] typ: variables file

---
+++ variables.sh
@@ -0,0 +1,10 @@
+export BASE_IMAGE="vauxoo/odoo-110-image"
+export PSQL_VERSION=10
+export VERSION="11.0"
+export ODOO_REPO="vauxoo/odoo"
+export ODOO_BRANCH="11.0"
+export MAIN_APP="typ"
+export TRAVIS_PYTHON_VERSION="3.5"
+export COUNTRY="MX"
+export CUSTOMER="typ"
+export INCLUDE="typ"
