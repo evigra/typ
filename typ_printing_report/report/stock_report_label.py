32ccc2814f1b07ad625217df856c36193cfbf95e
santonyytyp <jmartinez@typrefrigeracion.com>
2019-02-21 19:21:47 +0000

    [ADD] typ_printing_report: new labels

--- typ_printing_report/report/stock_report_label.py
+++ typ_printing_report/report/stock_report_label.py
@@ -30,9 +30,91 @@ class ReportZebraProduct(models.AbstractModel):
         }
 
 
+class ReportZebraProductM(models.AbstractModel):
+
+    _name = 'report.typ_printing_report.product_label_medium_zebra_view'
+
+    @api.model
+    def render_html(self, docids, data=None):
+        if data is None:
+            data = {}
+
+        datas = {
+            'docs': self.env['product.product'].browse(
+                data.get('ids', docids)),
+            'qty': data.get('qty', 1)
+        }
+        return self.env['report'].render(
+            'typ_printing_report.product_label_zebra_view', datas)
+
+    @api.multi
+    def get_report_values(self, docids, data):
+        products = self.env['product.product'].browse(docids)
+        return {
+            'doc_ids': products.ids,
+            'docs': products,
+            'data': data
+        }
+
+
+class ReportZebraProductE(models.AbstractModel):
+
+    _name = 'report.typ_printing_report.product_label_extra_zebra_view'
+
+    @api.model
+    def render_html(self, docids, data=None):
+        if data is None:
+            data = {}
+
+        datas = {
+            'docs': self.env['product.product'].browse(
+                data.get('ids', docids)),
+            'qty': data.get('qty', 1)
+        }
+        return self.env['report'].render(
+            'typ_printing_report.product_label_zebra_view', datas)
+
+    @api.multi
+    def get_report_values(self, docids, data):
+        products = self.env['product.product'].browse(docids)
+        return {
+            'doc_ids': products.ids,
+            'docs': products,
+            'data': data
+        }
+
+
 class ReportZebraRack(models.AbstractModel):
 
     _name = 'report.typ_printing_report.rack_label_zebra_view'
+
+    @api.multi
+    def get_report_values(self, docids, data):
+        products = self.env['product.product'].browse(docids)
+        return {
+            'doc_ids': products.ids,
+            'docs': products,
+            'data': data
+        }
+
+
+class ReportZebraRackM(models.AbstractModel):
+
+    _name = 'report.typ_printing_report.rack_label_medium_zebra_view'
+
+    @api.multi
+    def get_report_values(self, docids, data):
+        products = self.env['product.product'].browse(docids)
+        return {
+            'doc_ids': products.ids,
+            'docs': products,
+            'data': data
+        }
+
+
+class ReportZebraRackE(models.AbstractModel):
+
+    _name = 'report.typ_printing_report.rack_label_extra_zebra_view'
 
     @api.multi
     def get_report_values(self, docids, data):
