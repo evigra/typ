7b798c8d856828b0d63b935310df10933e110b8d
Deivis Laya <deivis@vauxoo.com>
2019-03-01 01:03:59 +0000

    [FIX] typ_account: avoid assigning a partner automatically

--- typ_account/models/account_bank_statement.py
+++ typ_account/models/account_bank_statement.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models
+from openerp import models, api
 from odoo.exceptions import AccessError
 
 
@@ -14,4 +14,21 @@ class AccountBankStatementLine(models.Model):
             res.check_access_rule('read')
         except AccessError:
             return self.env['account.move.line']
+        return res
+
+
+class AccountBankStatement(models.Model):
+    _inherit = "account.bank.statement"
+
+    @api.multi
+    def reconciliation_widget_preprocess(self):
+        """ Inherit method to avoid assigning a partner.
+        """
+        st_lines_old = [l.id for l in self.line_ids.filtered(
+            lambda l: not l.partner_id)]
+        res = super().reconciliation_widget_preprocess()
+        st_lines = self.env['account.bank.statement.line'].browse(
+            res['st_lines_ids'])
+        for st_line in st_lines.filtered(lambda l: l.id in st_lines_old):
+            st_line.partner_id = False
         return res
