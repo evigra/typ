fccb12e5ab9c0f8dc424b2608f9643642764d66a
Deivis Laya <deivis@vauxoo.com>
2019-05-30 23:09:51 +0000

    [ADD] typ_sale: Unit Test for automatic fiscal position in sale order

--- typ_sale/tests/__init__.py
+++ typ_sale/tests/__init__.py
@@ -7,3 +7,4 @@ from . import test_invoice_type_payment_term
 from . import test_sale_below_margin
 # from . import test_cancel_sale_order
 from . import test_sale_change_price_unit
+from . import test_position_fiscal_border_location
