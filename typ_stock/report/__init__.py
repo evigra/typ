4633dc3d39a8e41c32e910a7eee03872973f0869
santonyytyp <jmartinez@typrefrigeracion.com>
2019-08-13 18:40:33 +0000

    [ADD] typ_stock: New Report without barcodes

--- typ_stock/report/__init__.py
+++ typ_stock/report/__init__.py
@@ -3,3 +3,4 @@
 # from . import stock_analysis
 from . import report_stockpicking
 from . import stock_report_deliveryslip
+from . import report_stockpicking_wobc
---
+++ typ_stock/report/report_stockpicking_wobc.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+from odoo import api, models
+
+
+class StockPickingOperationsiReportWobc(models.AbstractModel):
+    _name = 'report.typ_stock.report_picking_wobc'
+
+    @api.multi
+    def get_report_values(self, docids, data=None):
+        docs = self.env['stock.picking'].browse(docids)
+        return {
+            'doc_ids': docs.ids,
+            'doc_model': 'stock.picking',
+            'docs': docs,
+            'data': data,
+            'lines_category_three': self.get_line_by_category_three
+        }
+
+    def get_line_by_category_three(self, doc):
+        moves = doc.mapped('move_lines')
+        group_move = defaultdict(lambda: self.env['stock.move'])
+        for move in moves:
+            complete_name = move.product_id.categ_id.complete_name
+            name_three = complete_name.rsplit("/", 3)
+            if len(name_three) > 3:
+                name_three.pop(0)
+            name_three.pop()
+            complete_name_three = "/".join(name_three).strip()
+            group_move[complete_name_three] |= move
+        return sorted(group_move.items())
