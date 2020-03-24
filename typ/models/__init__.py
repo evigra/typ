b11e43b6b42147b974709060002933222e5dafec
santonyytyp <jmartinez@typrefrigeracion.com>
2020-03-24 18:15:37 +0000

    [IMP]typ: helpdesk ticket add field product

--- typ/models/__init__.py
+++ typ/models/__init__.py
@@ -9,3 +9,4 @@ from . import account_move
 from . import stock_move_line
 from . import pos_order
 from . import account_journal
+from . import helpdesk_ticket
---
+++ typ/models/helpdesk_ticket.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from odoo import models, fields
+
+
+class HelpdeskTicket(models.Model):
+
+    _inherit = 'helpdesk.ticket'
+
+    product_id = fields.Many2one('product.product')
