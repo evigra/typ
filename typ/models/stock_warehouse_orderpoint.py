7d62cb40377c5cfa9a1eab2c1e4817f6019cc806
Luis González <lgonzalez@vauxoo.com>
2022-03-03 21:45:29 +0000

    [FIX] typ: don't consider lead times in the scheduler
    When the inventory scheduler is run, don't consider lead times
    computing forecasted quantities.
    
    For more info, see:
    
    https://github.com/odoo/odoo/commit/56efb55057b9

--- typ/models/stock_warehouse_orderpoint.py
+++ typ/models/stock_warehouse_orderpoint.py
@@ -7,3 +7,9 @@ class StockWarehouseOrderpoint(models.Model):
     importance = fields.Selection([("aa", "AA"), ("a", "A"), ("b", "B"), ("c", "C"), ("d", "D")])
     note = fields.Text()
     reorder_point = fields.Char()
+
+    def _get_product_context(self):
+        """Don't consider lead times when scheduler computes forecasted quantities"""
+        context = super()._get_product_context()
+        context.pop("to_date", None)
+        return context
