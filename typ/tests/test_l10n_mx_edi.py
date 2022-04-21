15cf7ed51eb9703a456cfc51b82d1b5bc7fe9c26
Luis González <lgonzalez@vauxoo.com>
2022-04-21 18:27:16 +0000

    [FIX] typ: ensure l10n tests run at post install
    A new validation was added in Odoo [1] to make sure all test classes
    hinheriting from account run at post install. This is because such tests
    require a CoA to be installed and were being (kind of) silently skipped
    when no COa could be found.
    
    [1] https://github.com/odoo/odoo/commit/81aac30dd227

--- typ/tests/test_l10n_mx_edi.py
+++ typ/tests/test_l10n_mx_edi.py
@@ -3,7 +3,7 @@ from odoo.tests import tagged
 from odoo.addons.l10n_mx_edi.tests import test_cfdi_xml
 
 
-@tagged("l10n_mx_edi")
+@tagged("l10n_mx_edi", "post_install", "-at_install")
 class TestEdiResults(test_cfdi_xml.TestEdiResults):
     """This run native tests from l10n_mx_edi.
 
