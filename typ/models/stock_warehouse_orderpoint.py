ae7695b4d0a9d0d2262c38c303da4b27ab8d14d1
JulioSerna <julio@vauxoo.com>
2022-05-09 21:37:20 +0000

    [IMP] typ: validate do not create other picking when pass the 1000 orderpoint batch when run scheduller
    Before this commit we force to create and not merge from confirm method of the
    stock.move but if the run scheduller found 1000 orderpoint create second
    batch and create another procurement
    
    After this commit we create procurement before processing the batch and
    we do not need to force the not merge because the move already has the
    procurement group before the confirm method

--- typ/models/stock_warehouse_orderpoint.py
+++ typ/models/stock_warehouse_orderpoint.py
@@ -13,3 +13,18 @@ class StockWarehouseOrderpoint(models.Model):
         context = super()._get_product_context()
         context.pop("to_date", None)
         return context
+
+    def _procure_orderpoint_confirm(self, use_new_cursor=False, company_id=None, raise_user_error=True):
+        group = self.env["procurement.group"].sudo().create({})
+        if use_new_cursor:
+            self._cr.commit()  # pylint: disable=invalid-commit
+        self_ctx = self.with_context(procurement_group=group)
+        res = super(StockWarehouseOrderpoint, self_ctx)._procure_orderpoint_confirm(
+            use_new_cursor=use_new_cursor, company_id=company_id, raise_user_error=raise_user_error
+        )
+        return res
+
+    def _prepare_procurement_values(self, date=False, group=False):
+        res = super()._prepare_procurement_values(date=date, group=group)
+        res["group_id"] = self._context.get("procurement_group")
+        return res
